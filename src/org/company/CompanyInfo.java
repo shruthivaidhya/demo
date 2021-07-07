1d3a967b221749b1c2723114726a115c35f78f43
shruthivaidhya <shruthivaidhiya95@gmail.com>
2021-07-07 14:30:52 +0530

    anitha modified

--- src/org/company/CompanyInfo.java
+++ src/org/company/CompanyInfo.java
@@ -2,28 +2,25 @@ package org.company;
 
 public class CompanyInfo {
 
-	public void companyName() {
+	public void companyId() {
 		System.out.println("Company Name : Infosys");
 	}
-		public void companyId() {
-			System.out.println("Company ID: ij456");
-		}
-		public void companyAddress() {
-			System.out.println("Company Address: Tidel Park, Chennai");
-		}
+		
+	
+	
+	
+	
+	
 		
 //Main Method
 		public static void main(String[] args) {
 			
 			//object
 			
-			CompanyInfo c=new CompanyInfo();
+			CompanyInfo d=new CompanyInfo();
 			
 			//method calling
-			c.companyName();
-			c.companyId();
-			c.companyAddress();
-			
+			d.companyId();
 		}
 		
 			
